d7b02e181e26af8fa91be24ff91ebdfa37ddcfd3
James Johnston <james@ezmuze.co.uk>
2020-09-09 23:58:08 +0100

    Commiting the correct driver

---
+++ GigabyteConfigModel.cs
@@ -0,0 +1,10 @@
+﻿using SimpleLed;
+
+namespace Driver.AorusIT8297
+{
+    public class GigabyteConfigModel : SLSConfigData
+    {
+        public int ARGB1Leds { get; set; }
+        public int ARGB2Leds { get; set; }
+    }
+}
---
+++ IT8297Config.cs
@@ -0,0 +1,8 @@
+﻿namespace Driver.AorusIT8297
+{
+    public class IT8297Config
+    {
+        public int Fan1LedCount { get; set; } = 21;
+        public int Fan2LedCount { get; set; } = 21;
+    }
+}
